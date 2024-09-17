615e1c99b63a8a111243d82cea460e63619c95a5
Stephen Rout <srr@routhouse.org>
2024-09-16 22:25:16 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -8,14 +8,13 @@ import sys
 
 def match_pattern(input_line, pattern):
     if len(pattern) == 1:
-        return pattern in input_line
-    elif pattern == "\\d":  
-        return any(c.isnumeric() for c in input_line)            
+        return char_pattern(input_line, pattern) #Each pattern has a function, for consitency
+    elif pattern == "\\d":
+        return numeric_pattern(input_line, pattern)
     elif pattern == "\\w":
-        return any((c.isnumeric() 
-        or c.isalpha() 
-        or c == "_") 
-        for c in input_line)
+        return alphanumeric_pattern(input_line)
+    elif pattern[0] == "[" and pattern[-1] == "]": #is this a bad place for input validation?
+        return positive_group_pattern(input_line, pattern)
     else:
         raise RuntimeError(f"Unhandled pattern: {pattern}")
 
@@ -38,6 +37,34 @@ def main():
     else:
         exit(1)
 
+
+
+def char_pattern(input_line, pattern):
+    '''Returns true if input_line contains pattern
+    '''
+    return pattern in input_line
+
+
+def numeric_pattern(input_line, pattern):
+    '''Returns true if input_line contains any numbers
+    '''
+    return any(c.isnumeric() for c in input_line)       
+
+
+def alphanumeric_pattern(input_line):
+    '''Returns true if input_line contains numbers, letters, or _
+    '''
+    return any((c.isnumeric() or c.isalpha() or c == "_") 
+        for c in input_line)
+
+def positive_group_pattern(input_line, pattern):
+    '''Returns true if input_line contains any characters from pattern
+    '''
+
+    pattern_set = set(pattern[1:-2])
+    input_set = set(input_line)
+
+    return not (pattern_set.isdisjoint(input_set))
 
 if __name__ == "__main__":
     main()
